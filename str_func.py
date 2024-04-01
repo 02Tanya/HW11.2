6f9191850df56bf11482017b61c5f21376394a96
TatyanaSh <tr3520@bk.ru>
2024-04-01 21:55:02 +0300

    Addition function for title and docstrings

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 def get_uppercase_phrase():
-
+    '''Делает буквы заглавными'''
     phrase = input('Введите фразу или предложение: ')
     return phrase.upper()
+
+
+def get_first_worldletter_up():
+    '''Повышает регистр первой буквы каждого слова'''
+    phrase = input('Введите фразу или предложение: ')
+    return phrase.title()
